89bbd498c99681cea6fc0b8c7453fc7ff911d463
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2024-10-26 21:05:31 +0900

    feat

---
+++ disable_demo.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+# 1. 関数名を取得してスペース区切りの文字列として保存
+# Azure CLI だと、StagingSlotの関数一覧を取得することができないので、REST APIを使う
+# https://stackoverflow.com/questions/75801784/how-can-i-list-azure-functionapp-functions-in-a-specific-deployment-slot-with-az
+FUNCTIONS=$(az rest --method get --uri "https://management.azure.com/subscriptions/ba29533e-1e4c-43a8-898a-a5815e9b577b/resourceGroups/tatsukoni-test-v2/providers/Microsoft.Web/sites/func-tatsukoni-test-v1/slots/staging/functions?api-version=2018-11-01" | \
+jq -r '.value[] | select(all(.properties.config.bindings[].type; . != "httpTrigger")) | .properties.name')
+
+# 2. 関数名を使ってアプリケーション設定の文字列を構築
+SETTINGS=$(echo "$FUNCTIONS" | while read -r func; do
+    echo "AzureWebJobs.${func}.Disabled=true "
+done)
+SETTINGS=$(echo $SETTINGS | sed -e 's/[[:space:]]*$//')
+
+# 3. Staging SlotのHttpTriggger以外の関数を無効化する
+az functionapp config appsettings set \
+  --name func-tatsukoni-test-v1 \
+  --resource-group tatsukoni-test-v2 \
+  --slot staging \
+  --slot-settings $SETTINGS
